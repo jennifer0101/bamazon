d26971ffd9b9eddab9d8e13e6f2f9ed31c2009ef
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-09-29 22:05:31 -0500

    select id logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,54 +8,61 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "4c!37t/6FKOZ",
     database: "bamazon"
   });
 
 
 connection.connect(function(err) {
     if (err) throw err;
-    selectItems();
-})
+    displayItems();
+});
 
-//Display all products for sale. Prompt user with two messages: 1. ID of the product. 2: How many units?
-function selectItems() {
+var tableData = "Select * FROM products";
+
+
+function displayItems() {
   //query database for products available.
-  connection.query("SELECT * FROM products", function (err, results){
+  connection.query(tableData, function (err, results){
     if (err) throw err;
     for (var i = 0; i < results.length; i++) {
-    console.log("Product Id: " + results[i].id + " || Product Name: " + results[i].product_name + " || Department: " + results[i].department_name + " || Price: " + results[i].price );
+      console.log("Product Id: " + results[i].id + "   Product Name: " + results[i].product_name + "       Department: " + results[i].department_name + "       Price: " + results[i].price );
     }
-    //prompt user for which they'd like to purchase.
-    inquirer
-      .prompt(
-        {
-          name: "select",
-          type: "list",
-          choices: function() {
-            var listArray = [];
-            for (var i = 0; i < results.length; i++){
-              listArray.push(results[i].id);
-            }
-            return listArray;
-          },
-          message: "What is the ID number of the item you would like to purchase?"
-        },
-        {
-          name: "id",
-          type: "input",
+    selectProduct();
+  })
+};
+
+function selectProduct() {
+  connection.query(tableData, function(err, results){
+    if (err) throw err;
+  inquirer
+  .prompt(
+    {
+      name: "id",
+      type: "rawlist",
+      choices: function() {
+        var productChoices = [];
+        for (var i = 0; i < results.length; i++) {
+          productChoices.push(results[i].id);
         }
-        )
-        .then(function(answer) {
-         
-        });
-          
-          
+        return productChoices;
+      },
+      message: "What is the ID number of the product you wish to purchase?"
+    //},
+    // {
+    //   name: "quantity",
+    //   type: "input",
+    //   message: "How many of these items to you wish to purchase?"
+    // },
+  //])
+})
+}
 
-         
-        })
-  }
+  // }
 
+    //Display all products for sale. Prompt user with two messages: 1. ID of the product. 2: How many units?
+    //prompt user for which they'd like to purchase.
+    
 
 
 
@@ -66,4 +73,4 @@ function selectItems() {
 
 //If there is enough in stock, fulfill customer oder by:
   //Updating SQL database to reflect the reamining quantity.
-  //Once update goes through, show the customer the total cost of their purchase.
+  //Once update goes through, show the customer the total cost of their purchase
