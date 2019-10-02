31768e43b804b727e1f9af3fbb26eacf17872ff3
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-10-01 23:04:27 -0500

    logic for id selector

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,17 +5,17 @@ var inquirer = require("inquirer");
 
 //connect to mysql database
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "4c!37t/6FKOZ",
-    database: "bamazon"
-  });
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "4c!37t/6FKOZ",
+  database: "bamazon"
+});
 
 
-connection.connect(function(err) {
-    if (err) throw err;
-    displayItems();
+connection.connect(function (err) {
+  if (err) throw err;
+  displayItems();
 });
 
 var tableData = "Select * FROM products";
@@ -23,50 +23,68 @@ var tableData = "Select * FROM products";
 
 function displayItems() {
   //query database for products available.
-  connection.query(tableData, function (err, results){
+  connection.query(tableData, function (err, results) {
     if (err) throw err;
-    for (var i = 0; i < results.length; i++) {
-      console.log("Product Id: " + results[i].id + "   Product Name: " + results[i].product_name + "       Department: " + results[i].department_name + "       Price: " + results[i].price );
-    }
-    selectProduct();
+    console.table(results);
+    selectProduct(results);
   })
 };
 
-function selectProduct() {
-  connection.query(tableData, function(err, results){
-    if (err) throw err;
+//Display all products for sale. Prompt user with two messages: 1. ID of the product. 2: How many units?
+function selectProduct(results) {
+  // connection.query(tableData, function(err, results){
+  //   if (err) throw err;
+  var productChoices = [];
+  for (var i = 0; i < results.length; i++) {
+    productChoices.push(results[i].product_name);
+  }
+
   inquirer
-  .prompt(
-    {
-      name: "id",
-      type: "rawlist",
-      choices: function() {
-        var productChoices = [];
-        for (var i = 0; i < results.length; i++) {
-          productChoices.push(results[i].id);
-        }
-        return productChoices;
-      },
-      message: "What is the ID number of the product you wish to purchase?"
-    //},
-    // {
-    //   name: "quantity",
-    //   type: "input",
-    //   message: "How many of these items to you wish to purchase?"
-    // },
-  //])
-})
+    .prompt([
+      {
+        name: "productName",
+        type: "rawlist",
+        choices: productChoices,
+        message: "What is the ID number of the product you wish to purchase?"
+      }
+    ])
+    .then(function (answer) {
+      var chosenItem = answer.productName;
+      
+      console.log("We chose: ", chosenItem);
+
+      // How many do you want to purchase?
+      //    Get quantity from database OR, since it's still in memory (above), look at that
+
+      // User inputs a number
+
+      // If number is less than or equal to quantity, subtract and update record
+      // If not, tell them to do it again but better this time
+    });
 }
 
-  // }
+function quantityPrompt(qty) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "stock_quantity",
+        message: "How many units of this item do you want to purchase?"
+      }
+    ])
+    .then(function (answer) {
+      if (answer.stock_quantity <= qty) {
+        console.log("aa?");
+      }
+    });
+}
 
-    //Display all products for sale. Prompt user with two messages: 1. ID of the product. 2: How many units?
-    //prompt user for which they'd like to purchase.
-    
 
-
-
-  
+// {
+//   name: "quantity",
+//   type: "input",
+//   message: "How many of these items to you wish to purchase?"
+// },
 
 //Check if store has enough of product to meet customer request.
   //If not enough stock, log phrase "Insufficient quantity!" and prevent the order form going through.
