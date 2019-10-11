251b0d00d15f4e6b4cd30eb2ec3772f1f918ee5a
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-10-11 17:14:18 -0500

    select id, input stock quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,92 +2,161 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-
 //connect to mysql database
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "4c!37t/6FKOZ",
   database: "bamazon"
 });
 
-
 connection.connect(function (err) {
   if (err) throw err;
-  displayItems();
+  displayTable();
 });
 
 var tableData = "Select * FROM products";
 
-
-function displayItems() {
+//display table
+function displayTable() {
   //query database for products available.
   connection.query(tableData, function (err, results) {
     if (err) throw err;
+    for (var i = 0; i < results.length; i++) {
+      
+    }
     console.table(results);
     selectProduct(results);
-  })
-};
+  });
+}
 
-//Display all products for sale. Prompt user with message what ID of product they want.
+//Prompt user with message what ID of product they want.
 function selectProduct(results) {
-  var productChoices = [];
-  for (var i = 0; i < results.length; i++) {
-    productChoices.push(results[i].id + "  " + results[i].product_name + "  " + results[i].department_name + "  " + results[i].price);
+  // connection.query(tableData, function (err, results) {
+  //   if (err) throw err;
+    inquirer
+      .prompt(
+        {
+          name: "userId",
+          type: "input",
+          message: "Which product would you like to purchase?",
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        })
+        
+      .then(function (answer) {
+        //var chosenId = answer.userId;
+        var id = parseInt(answer.userId);
+        var product = checkInventory(id, results);
+        if (product){
+          console.log(product);
+          quantityPrompt(product);
+        } else {
+          //console.log(product);
+          console.log("product does not exist")
+        }
+      }
+        
+      
+  )}
+
+  function checkInventory(id, results) {
+    for (var i = 0; i < results.length; i++) {
+      if (id === results[i].id) {
+        return results[i];
+      } 
+    }
+    return null;
   }
+       
+  
+  //Promt user with question of how many units they want?
+function quantityPrompt(product) {
   inquirer
-    .prompt([
-      {
-        name: "productName",
-        type: "rawlist",
-        choices: productChoices,
-        message: "Which product would you like to purchase?"
-      },
-      {
+    .prompt({
         type: "input",
         name: "stock_quantity",
         message: "How many units of this item do you want to purchase?"
-      },
-    ])
+      })
     .then(function (answer) {
-      if (productChoices < answer.stock_quantity){
-        console.log(productChoices);
-      //   console.log("yay");
-      // } else {
-      //   console.log("boo");
+      //console.log(answer.stock_quantity);
+      //console.log(answer);
+      var quantity = parseInt (answer.stock_quantity);
+      if (quantity > product.stock_quantity){
+        console.log("insufient quantity");
+        displayTable();
+      } else {
+        finalizePurchase();
       }
-      // answer = answer.productName;
-      // chosenItem = results[i].stock_quantity;
-      // console.log(chosenItem);
-      // quantityPrompt();
-    });
-}
+    })
+  }
+             
+    // for (var i = 0; i < results.length; i++) {
+    //  if (chosenId === results[i].id){
+    //   console.log (results[i].id + "  " + results[i].product_name + "  " + results[i].department_name + "  " + results[i].price);
 
-//Promt user with question of how many units they want?
-// function quantityPrompt(chosenItem) {
-//   inquirer
-//     .prompt({
-//         type: "input",
-//         name: "stock_quantity",
-//         message: "How many units of this item do you want to purchase?"
-//       })
-//     .then(function (answer) {
-//       //console.log(answer.stock_quantity);
-//       console.log(chosenItem);
+    //  for (var i = 0; i < results.length; i++) {
+    //   if (answer.userId === results[i].id){
+    //     console.log (results[i].id + "  " + results[i].product_name + "  " + results[i].department_name + "  " + results[i].price);
+    //    } else {
+    //      console.log ("error yo")
+    //    }
       
-      
-      
+//   {
+//     name: "userQuantity",
+//     type: "input",
+//     message: "How many of these items do you wish to purchase?",
+//     validate: function(value) {
+//       if (isNaN(value) === false) {
+//         return true;
+//       }
+//       return false;
+//     }
+//   }
+// )
+    //var productChoices = answer.id; 
+    //{
+      // id: results[i].id,
+      // product_name: results[i].product_name,
+      // department_name: results[i].department_name,
+      // price: results[i].price,
+      // stock_quantity: results[i].stock_quantiy
+    //}
+
+      // {
+      //   type: "input",
+      //   name: "stock_quantity",
+      //   message: "How many units of this item do you want to purchase?"
+      // },
+
+    // .then(function (answer) {
+    //   console.log(answer)
+    //   if (productChoices[productName].id < answer.stock_quantity){
+    //     // console.log(productChoices);
+    //     console.log("yay");
+    //   } else {
+    //     console.log("boo");
+    //   }
+    //   // answer = answer.productName;
+    //   // chosenItem = results[i].stock_quantity;
+    //   // console.log(chosenItem);
+    //   // quantityPrompt();
+    // });
+//}
+
+
+
       //if (results.stock_quantity <= chosenItem.stock_quantity) {
-      
+
     //   console.log("aa?");
     //   });
 
     // }
-
-
-
-
 
 // How many do you want to purchase?
 //    Get quantity from database OR, since it's still in memory (above), look at that
@@ -96,10 +165,6 @@ function selectProduct(results) {
 
 // If number is less than or equal to quantity, subtract and update record
 // If not, tell them to do it again but better this time
-
-
-
-
 
 // {
 //   name: "quantity",
@@ -113,3 +178,12 @@ function selectProduct(results) {
 //If there is enough in stock, fulfill customer oder by:
   //Updating SQL database to reflect the reamining quantity.
   //Once update goes through, show the customer the total cost of their purchase
+
+  // var optionsChoice = {
+      //   id: results[i].id,
+      //   name: results[i].product_name,
+      //   department: results[i].department_name,
+      //   price: results[i].price,
+      //   quantity: results[i].stock_quantity
+      // }
+      
