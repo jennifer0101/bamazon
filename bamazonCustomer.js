da03bdde27a4b4d5a7952df97dd9e66385ac3e87
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-10-17 16:57:44 -0500

    cleaned up code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,7 +24,6 @@ function displayTable() {
   connection.query(tableData, function (err, results) {
     if (err) throw err;
     for (var i = 0; i < results.length; i++) {
-      
     }
     console.table(results);
     selectProduct(results);
@@ -33,8 +32,6 @@ function displayTable() {
 
 //Prompt user with message what ID of product they want.
 function selectProduct(results) {
-  // connection.query(tableData, function (err, results) {
-  //   if (err) throw err;
     inquirer
       .prompt(
         {
@@ -54,15 +51,13 @@ function selectProduct(results) {
         var id = parseInt(answer.userId);
         var product = checkInventory(id, results);
         if (product){
-          console.log(product);
+          console.table(product);
           quantityPrompt(product);
         } else {
           //console.log(product);
           console.log("product does not exist")
         }
-      }
-        
-      
+      } 
   )}
 
   function checkInventory(id, results) {
@@ -74,8 +69,7 @@ function selectProduct(results) {
     return null;
   }
        
-  
-  //Promt user with question of how many units they want?
+//Promt user with question of how many units they want?
 function quantityPrompt(product) {
   inquirer
     .prompt({
@@ -95,95 +89,4 @@ function quantityPrompt(product) {
       }
     })
   }
-             
-    // for (var i = 0; i < results.length; i++) {
-    //  if (chosenId === results[i].id){
-    //   console.log (results[i].id + "  " + results[i].product_name + "  " + results[i].department_name + "  " + results[i].price);
-
-    //  for (var i = 0; i < results.length; i++) {
-    //   if (answer.userId === results[i].id){
-    //     console.log (results[i].id + "  " + results[i].product_name + "  " + results[i].department_name + "  " + results[i].price);
-    //    } else {
-    //      console.log ("error yo")
-    //    }
-      
-//   {
-//     name: "userQuantity",
-//     type: "input",
-//     message: "How many of these items do you wish to purchase?",
-//     validate: function(value) {
-//       if (isNaN(value) === false) {
-//         return true;
-//       }
-//       return false;
-//     }
-//   }
-// )
-    //var productChoices = answer.id; 
-    //{
-      // id: results[i].id,
-      // product_name: results[i].product_name,
-      // department_name: results[i].department_name,
-      // price: results[i].price,
-      // stock_quantity: results[i].stock_quantiy
-    //}
-
-      // {
-      //   type: "input",
-      //   name: "stock_quantity",
-      //   message: "How many units of this item do you want to purchase?"
-      // },
-
-    // .then(function (answer) {
-    //   console.log(answer)
-    //   if (productChoices[productName].id < answer.stock_quantity){
-    //     // console.log(productChoices);
-    //     console.log("yay");
-    //   } else {
-    //     console.log("boo");
-    //   }
-    //   // answer = answer.productName;
-    //   // chosenItem = results[i].stock_quantity;
-    //   // console.log(chosenItem);
-    //   // quantityPrompt();
-    // });
-//}
-
-
-
-      //if (results.stock_quantity <= chosenItem.stock_quantity) {
-
-    //   console.log("aa?");
-    //   });
-
-    // }
-
-// How many do you want to purchase?
-//    Get quantity from database OR, since it's still in memory (above), look at that
-
-// User inputs a number
-
-// If number is less than or equal to quantity, subtract and update record
-// If not, tell them to do it again but better this time
-
-// {
-//   name: "quantity",
-//   type: "input",
-//   message: "How many of these items to you wish to purchase?"
-// },
-
-//Check if store has enough of product to meet customer request.
-  //If not enough stock, log phrase "Insufficient quantity!" and prevent the order form going through.
-
-//If there is enough in stock, fulfill customer oder by:
-  //Updating SQL database to reflect the reamining quantity.
-  //Once update goes through, show the customer the total cost of their purchase
-
-  // var optionsChoice = {
-      //   id: results[i].id,
-      //   name: results[i].product_name,
-      //   department: results[i].department_name,
-      //   price: results[i].price,
-      //   quantity: results[i].stock_quantity
-      // }
-      
+         
